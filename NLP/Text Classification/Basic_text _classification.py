74f80628fa4abb96178173d1fa7c115e4adb7841
andrei-obj <andrei.iancu@getobjectiv.com>
2021-04-16 14:28:00 +0200

    Add function to remove blank strings

--- NLP/Text Classification/Basic_text _classification.py
+++ NLP/Text Classification/Basic_text _classification.py
@@ -1,8 +1,11 @@
 import numpy as np
 import pandas as pd
-from auxiliary_functions import check_categorical_columns, df_info
+from auxiliary_functions import check_categorical_columns, df_info, remove_blanks
 
 df = pd.read_csv('moviereviews.tsv', sep='\t')
 
 df_info(df)
 check_categorical_columns(df)
+remove_blanks(df)
+
+print(f'The cleaned df has shape {df.shape}.')
--- NLP/Text Classification/auxiliary_functions.py
+++ NLP/Text Classification/auxiliary_functions.py
@@ -1,5 +1,5 @@
 def df_info(df):
-    print(f'The shape of the dataframe is {df.shape}')
+    print(f'The initial shape of the dataframe is {df.shape}')
     print('\n-----------------------------------------\n')
     print(f'The types of the columns are:\n\n{df.dtypes}')
     print('\n-----------------------------------------\n')
@@ -15,10 +15,17 @@ def df_info(df):
 def check_categorical_columns(df):
     categorical_columns = df.select_dtypes(include=['object']).columns.tolist()
 
+    print('Info about empty/space strings in each categorical column:\n\n')
     for col in categorical_columns:
         entirely_empty_strings_count = (df[col] == '').sum()
         space_string_count = df[col].str.isspace().sum()
-        print(f'Column {col} has {df[col].isnull().sum()} nulls \
-              and {entirely_empty_strings_count} entirely empty strings \
-              and {space_string_count} only space strings')
-        print('\n')
+        print(f'Column {col} has {entirely_empty_strings_count} entirely empty strings and {space_string_count} only space strings')
+    print('\n-----------------------------------------\n')
+
+def remove_blanks(df):
+    blanks = []
+    for i, lb, rv in df.itertuples():
+        if rv.isspace():
+            blanks.append(i)
+    df.drop(blanks, inplace=True)
+    return df
